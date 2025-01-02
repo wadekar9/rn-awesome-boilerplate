c36c9f7b54e89baeb2ebb9e0eaae1939e5bf5756
wadekar9 <npwadekar7@gmail.com>
2025-01-02 18:07:13 +0530

    feat: add NoInternetConnectionPage component for improved user experience during connectivity issues

---
+++ template/src/components/pages/index.ts
@@ -0,0 +1,2 @@
+export { default as EmptyStateSage } from './empty-state-page.component';
+export { default as NoInternetConnectionPage } from './no-internet-connection-page.component';
---
+++ template/src/components/pages/no-internet-connection-page.component.tsx
@@ -0,0 +1,73 @@
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { ThemedView } from '$components/containers'
+import { EFonts, EFontSize, moderateScale } from '$constants/styles.constants'
+import { ICONS } from '$assets/images'
+import { ITheme } from '$types/common'
+import { useAppTheme } from '$hooks/common'
+import { Colors } from '$constants/colors.constants'
+import { BaseButton } from '$components/ui'
+import { ReloadOutlineIcon } from '$assets/icons'
+
+const NoInternetConnectionPage : React.FC = () => {
+
+  const { theme, colors } = useAppTheme();
+  const styles = styling(theme);
+
+  return (
+    <ThemedView>
+      <View style={styles.container}>
+        <Image
+          source={ICONS.NETWORK_CONNECTION}
+          style={{ width: moderateScale(150), height: moderateScale(150) }}
+        />
+
+        <View style={styles.content}>
+          <Text style={styles.label}>No internet connection!</Text>
+          <Text style={styles.description}>Please check your network connection!</Text>
+          <BaseButton
+            label='Try Again'
+            LeftAccessory={<ReloadOutlineIcon width={moderateScale(20)} height={moderateScale(20)} />}
+            containerStyle={styles.buttonContainer}
+            labelStyle={{ color: colors.text }}
+          />
+        </View>
+      </View>
+    </ThemedView>
+  )
+}
+
+export default NoInternetConnectionPage
+
+const styling = (theme : ITheme) => StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: moderateScale(50),
+    padding : moderateScale(20),
+  },
+  content : {
+    alignItems : 'center',
+    justifyContent : 'center',
+    gap : moderateScale(15),
+    width : '100%'
+  },
+  label : {
+    fontFamily : EFonts.SEMI_BOLD,
+    fontSize : EFontSize['2XL'],
+    color : Colors[theme].text
+  },
+  description : {
+    fontFamily : EFonts.MEDIUM,
+    fontSize : EFontSize.LG,
+    color : Colors[theme].grey
+  },
+  buttonContainer : {
+    width : 'auto',
+    alignSelf : 'center',
+    paddingHorizontal : moderateScale(22),
+    borderWidth : StyleSheet.hairlineWidth,
+    backgroundColor : 'transparent',
+  }
+})
