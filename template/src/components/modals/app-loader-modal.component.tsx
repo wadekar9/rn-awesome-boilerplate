475b52b1070e43d5e3638a1e766fa5a6d9530811
wadekar9 <npwadekar7@gmail.com>
2025-01-02 17:22:27 +0530

    feat: add loader modal component for loading indication

---
+++ template/src/components/modals/app-loader-modal.component.tsx
@@ -0,0 +1,32 @@
+import { ActivityIndicator, Modal, StyleSheet, View } from 'react-native';
+import React, { memo } from 'react';
+import { Colors } from '$constants/colors.constants';
+
+const AppLoaderModal: React.FC = () => {
+
+    return (
+        <Modal
+            transparent
+            animationType='fade'
+            visible={true}
+            statusBarTranslucent
+        >
+            <View style={[styles.container, { backgroundColor: 'rgba(0,0,0,0.3)' }]}>
+                <ActivityIndicator size={'large'} color={Colors.light.primary} />
+            </View>
+        </Modal>
+    )
+}
+
+export default memo(AppLoaderModal);
+
+const styles = StyleSheet.create({
+    wrapper: {
+        flex: 1,
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
---
+++ template/src/components/modals/index.ts
@@ -0,0 +1,1 @@
+export { default as AppLoaderModal } from './app-loader-modal.component';
