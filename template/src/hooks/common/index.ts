b374c02e34b14720ab621192f79a6f3bf0b6f70c
wadekar9 <npwadekar7@gmail.com>
2025-01-02 17:24:39 +0530

    feat: update hook exports to include app theme and translation hooks

--- template/src/hooks/common/index.ts
+++ template/src/hooks/common/index.ts
@@ -1,5 +1,7 @@
+export { useAppTheme } from './app-theme.hook';
+export { useAppTranslation } from './app-translation.hook';
 export { useDebounce } from './debounce.hook';
 export { useDocumentPicker } from './document-picker.hook';
 export { useImagePicker } from './image-picker.hook';
-export { useSafeAreaInsetsStyle } from './safearea-inset-styles.hook';
+export { useSafeAreaInsetsStyle } from './safearea-styles.hook';
 export { } from './store-dispatch-selector.hook';
