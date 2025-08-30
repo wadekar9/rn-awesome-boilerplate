8dfcd77d14683376b0fab36baa9cae99ef6eeb08
wadekar9 <npwadekar7@gmail.com>
2025-08-30 23:13:23 +0530

    fix : update in app theme configuration

--- template/app/context/app-theme.context.tsx
+++ template/app/context/app-theme.context.tsx
@@ -1,38 +1,45 @@
-import React, { createContext, useEffect, useMemo, useState } from 'react'
-import { getData, storeData, storage } from '$utils/storage';
+import React, { createContext, useCallback, useEffect, useMemo, useState } from 'react';
+import { getData, storeData } from '$utils/storage';
 import { EStorageKeys } from '$constants/storage.constants';
 import { AppThemeContextProps, ITheme } from '$types/common';
 import { useColorScheme, Appearance } from 'react-native';
 
-export const AppThemeContext = createContext<AppThemeContextProps | undefined>(undefined)
+export const AppThemeContext = createContext<AppThemeContextProps | undefined>(undefined);
 
 const AppThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 
-    const systemTheme = useColorScheme() ?? 'light'
+    const systemTheme = useColorScheme() ?? 'light';
     const [appTheme, setAppTheme] = useState<ITheme>(systemTheme);
 
-    function changeTheme(theme: ITheme) {
+    const changeTheme = useCallback((theme: ITheme) => {
         try {
-            setAppTheme(theme)
+            setAppTheme(theme);
             storeData(EStorageKeys.APP_THEME, theme);
         } catch (err) {
-            console.log("ERROR: ", err);
+            console.log('ERROR: ', err);
         }
-    }
+    }, []);
 
     useEffect(() => {
         (async () => {
             const result = await getData(EStorageKeys.APP_THEME);
             if (result) {
-                setAppTheme(result as ITheme);
+                if (result === 'default') {
+                    setAppTheme(systemTheme);
+                } else {
+                    setAppTheme(result as ITheme);
+                }
             }
-        })()
-    }, []);
+        })();
+    }, [systemTheme]);
 
     useEffect(() => {
-        const listener = Appearance.addChangeListener(({ colorScheme }) => setAppTheme(colorScheme as ITheme));
+        const listener = Appearance.addChangeListener(({ colorScheme }) => {
+            storeData(EStorageKeys.APP_THEME, colorScheme);
+            setAppTheme(colorScheme as ITheme);
+        });
         return () => listener.remove();
-    }, [])
+    }, [systemTheme]);
 
     const contextValue = useMemo(() => ({ currentTheme: appTheme, changeTheme }), [appTheme, changeTheme]);
 
@@ -40,7 +47,7 @@ const AppThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children })
         <AppThemeContext.Provider value={contextValue}>
             {children}
         </AppThemeContext.Provider>
-    )
-}
+    );
+};
 
-export default AppThemeProvider
+export default AppThemeProvider;
