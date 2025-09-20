059f5226c85855b4687ff1f61dc23149872b6f56
wadekar9 <npwadekar7@gmail.com>
2025-09-20 18:45:41 +0530

    refactor : add app provides to the root file

--- template/App.tsx
+++ template/App.tsx
@@ -1,9 +1,29 @@
-import RootStackNavigator from '$navigation/root-stack-navigator';
 import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import AppThemeProvider from '$context/app-theme.context';
+import { KeyboardProvider } from 'react-native-keyboard-controller';
+import { Provider as StoreProvider } from 'react-redux';
+import store from '$store/redux.store';
+import FlashMessage from 'react-native-flash-message';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import RootStackNavigator from '$navigation/root-stack-navigator';
+import { container } from '$styles/flexbox';
 
 const App = () => {
   return (
-    <RootStackNavigator />
+    <AppThemeProvider>
+      <StoreProvider store={store}>
+        <KeyboardProvider>
+          <SafeAreaProvider>
+            <GestureHandlerRootView style={container}>
+              <RootStackNavigator />
+            </GestureHandlerRootView>
+          </SafeAreaProvider>
+        </KeyboardProvider>
+      </StoreProvider>
+
+      <FlashMessage position="top" />
+    </AppThemeProvider>
   );
 };
 
